1c4aeb66aadf67bf9ff84b689df5907aca4d8a0d
kristinevella <12890998@student.uts.edu.au>
2022-04-06 17:29:00 +1000

    Add MoveableObject and logging classes
    As used in Assignment 1

---
+++ MoveableObject.m
@@ -0,0 +1,46 @@
+classdef MoveableObject < handle
+    %MOVEABLEOBJECTS A way of creating a group of moveable objects
+    %   The objects are created from .ply files
+       
+    properties     
+        %> Mesh of the object
+        mesh;
+
+        %> Vertices of the object
+        vertices;
+
+        %> Initial location of the object
+        currentLocation;
+
+        %> Goal location of the object
+        goalLocation;
+    end
+    
+    methods
+        %% ...structors
+        function self = MoveableObject(fileName)
+            self.mesh = PlaceObject(fileName);
+            self.vertices = get(self.mesh,'Vertices');
+        end   
+        
+        %% Move
+        % Move the object
+        function Move(self,location)
+            transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * location';
+            set(self.mesh,'Vertices',transformedVertices(:,1:3));
+            self.SetCurrentLocation(location);
+        end    
+
+        %% SetCurrentLocation
+        function SetCurrentLocation(self,location)
+            self.currentLocation = location;
+        end 
+        
+        %% SetGoalLocation
+        function SetGoalLocation(self,location)
+            self.goalLocation = location;
+        end 
+
+    end
+end
+
