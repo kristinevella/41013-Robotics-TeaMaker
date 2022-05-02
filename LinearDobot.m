9cb7ad9a58eafad4819624dd231671545b7118f9
kristinevella <12890998@student.uts.edu.au>
2022-05-03 02:10:44 +1000

    Position robot model in environment
    Copied files to main folder, edited joint limits and offset, set base position on bench

--- LinearDobot.m
+++ LinearDobot.m
@@ -1,7 +1,5 @@
 classdef LinearDobot < handle
     properties
-        L = SingleInstance.Logger;
-
         %> Robot model
         model;
         
@@ -12,7 +10,7 @@ classdef LinearDobot < handle
         useGripper = false;        
     end
     
-    methods%% Class for UR5 robot simulation
+    methods%% Class for Dobot robot simulation
 function self = LinearDobot(useGripper)
     self.useGripper = useGripper;
     
@@ -22,43 +20,52 @@ function self = LinearDobot(useGripper)
 % robot = 
 self.GetDobotRobot();
 % robot = 
-%self.PlotAndColourRobot();
-self.model.plot(zeros(1,self.model.n),'noarrow','workspace',self.workspace); %% Temporary until robot model files are included (@Juan)
+self.PlotAndColourRobot();%robot,workspace);
 end
 
 %% GetDobotRobot
-% Given a name (optional), create and return a UR5 robot model
+% Given a name (optional), create and return a Dobot robot model
 function GetDobotRobot(self)
 %     if nargin < 1
         % Create a unique name (ms timestamp after 1ms pause)
         pause(0.001);
-        name = ['LinearDobot_',datestr(now,'yyyymmddTHHMMSSFFF')];
+        name = ['Dobot_',datestr(now,'yyyymmddTHHMMSSFFF')];
 %     end
+    L(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
+    L(2) = Link('d',0.103,    'a',0,      'alpha',-pi/2,  'offset',0, 'qlim',[deg2rad(-135),deg2rad(135)]);
+    L(3) = Link('d',0,        'a',0.135,  'alpha',0,      'offset',-pi/2, 'qlim',[deg2rad(5),deg2rad(80)]);
+    L(4) = Link('d',0,        'a',0.147,  'alpha',0,      'offset',0, 'qlim',[deg2rad(15),deg2rad(170)]);
+    L(5) = Link('d',0,        'a',0.06,      'alpha',-pi/2,  'offset',-pi/2, 'qlim',[deg2rad(-90),deg2rad(90)]);
+    L(6) = Link('d',0.1,      'a',0,      'alpha',0,      'offset',0, 'qlim',[deg2rad(-85),deg2rad(85)]);
+
+    L(1).qlim = [-0.8 0]; % PRISMATIC Link
 
     % Create the Dobot model mounted on a linear rail
-    %% These are from the linear UR5 and need to be changed!!!!
-    L(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
-    L(2) = Link([0      0.1599  0       -pi/2   0]);
-    L(3) = Link([0      0.1357  0.425   -pi     0]);
-    L(4) = Link([0      0.1197  0.39243 pi      0]);
-    L(5) = Link([0      0.093   0       -pi/2   0]);
-    L(6) = Link([0      0       0       0	    0]);
+%     L(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
+%     L(2) = Link([0      0.1599  0       -pi/2   0]);
+%     L(3) = Link([0      0.1357  0.425   -pi     0]);
+%     L(4) = Link([0      0.1197  0.39243 pi      0]);
+%     L(5) = Link([0      0.093   0       -pi/2   0]);
+%     L(6) = Link([0      0.093   0       -pi/2	0]);
+%     L(7) = Link([0      0       0       0       0]);
     
-    % Incorporate joint limits (Suggested limits from notes on Canvas)
-    L(1).qlim = [-0.8 0]; % PRISMATIC Link
-    L(2).qlim = [-135 135]*pi/180;
-    L(3).qlim = [5 80]*pi/180;
-    L(4).qlim = [15 170]*pi/180;
-    L(5).qlim = [-90 90]*pi/180;
-    L(6).qlim = [-85 85]*pi/180;
-
-    L(3).offset = -pi/2;
+    % Incorporate joint limits
+%     L(1).qlim = [-0.8 0];
+%     L(2).qlim = [-360 360]*pi/180;
+%     L(3).qlim = [-90 90]*pi/180;
+%     L(4).qlim = [-170 170]*pi/180;
+%     L(5).qlim = [-360 360]*pi/180;
+%     L(6).qlim = [-360 360]*pi/180;
+%     L(7).qlim = [-360 360]*pi/180;
+% 
+%     L(3).offset = -pi/2;
+%     L(5).offset = -pi/2;
     
     self.model = SerialLink(L,'name',name);
     
-    % Rotate robot to the correct orientation and position
-    self.model.base = self.model.base * trotx(pi/2) * troty(pi/2);
-    self.L.mlog = {self.L.DEBUG,mfilename('class'),['GetDobotRobot: ','A Linear Dobot robot model has been created']};
+    % Rotate robot to the correct orientation
+    %self.model.base = self.model.base * trotx(pi/2) * troty(pi/2);
+    self.model.base = transl(-0.7,-3,1) * trotx(pi/2);
 end
 %% PlotAndColourRobot
 % Given a robot index, add the glyphs (vertices and faces) and
@@ -66,16 +73,17 @@ end
 function PlotAndColourRobot(self)%robot,workspace)
     for linkIndex = 0:self.model.n
         if self.useGripper && linkIndex == self.model.n
-            [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['LinDobotLink',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
+            [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['DobotLink',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
         else
-            [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['LinDobotLink',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+            [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['DobotLink',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
         end
         self.model.faces{linkIndex+1} = faceData;
         self.model.points{linkIndex+1} = vertexData;
     end
 
     % Display robot
-    self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+    %self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+    self.model.plot3d([0,0,5,15,0,0],'noarrow','workspace',self.workspace);
     if isempty(findobj(get(gca,'Children'),'Type','Light'))
         camlight
     end  
@@ -91,12 +99,10 @@ function PlotAndColourRobot(self)%robot,workspace)
                                                           , plyData{linkIndex+1}.vertex.blue]/255;
             h.link(linkIndex+1).Children.FaceColor = 'interp';
         catch ME_1
-            %disp(ME_1);
-            self.L.mlog = {self.L.WARN,mfilename('class'),['PlotAndColourRobot: ','There was a warning',self.L.ExceptionToString(ME_1)]};
+            disp(ME_1);
             continue;
         end
     end
-    self.L.mlog = {self.L.DEBUG,mfilename('class'),['PlotAndColourRobot: ','The Linear Dobot has been plotted']};
 end        
     end
 end
