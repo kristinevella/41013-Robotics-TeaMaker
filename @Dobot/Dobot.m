c94279ec3a8eba2676bb80f74929ea3a06bb6734
kristinevella <12890998@student.uts.edu.au>
2022-05-04 16:19:41 +1000

    Update Dobot.m
    Corrected limits per pdf on canvas

--- @Dobot/Dobot.m
+++ @Dobot/Dobot.m
@@ -36,11 +36,11 @@ function GetDobotRobot(self)
 %     end
     L(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link - copied from the LinearUR5 class
     L(1).qlim = [-1.33 0];
-    L(2) = Link('d',0.103+0.0362,    'a',0,      'alpha',-pi/2,  'offset',0, 'qlim',[deg2rad(-90),deg2rad(90)]);
-    L(3) = Link('d',0,        'a',0.135,  'alpha',0,      'offset',0, 'qlim',[deg2rad(-85),deg2rad(0)]);
-    L(4) = Link('d',0,        'a',0.147,  'alpha',0,      'offset',0, 'qlim',[deg2rad(-10),deg2rad(90)]);
-    L(5) = Link('d',0,        'a',0.06,      'alpha',-pi/2,  'offset',0, 'qlim',[deg2rad(-180),deg2rad(180)]);
-    L(6) = Link('d',0.1,      'a',0,      'alpha',0,      'offset',0, 'qlim',[deg2rad(-90),deg2rad(90)]);
+    L(2) = Link('d',0.103+0.0362,    'a',0,      'alpha',-pi/2,  'offset',0, 'qlim',[deg2rad(-135),deg2rad(135)]);
+    L(3) = Link('d',0,        'a',0.135,  'alpha',0,      'offset',-pi/2, 'qlim',[deg2rad(5),deg2rad(80)]);
+    L(4) = Link('d',0,        'a',0.147,  'alpha',0,      'offset',0, 'qlim',[deg2rad(15),deg2rad(170)]);
+    L(5) = Link('d',0,        'a',0.06,      'alpha',-pi/2,  'offset',0, 'qlim',[-pi/2,pi/2]);
+    L(6) = Link('d',0.1,      'a',0,      'alpha',0,      'offset',0, 'qlim',[deg2rad(-85),deg2rad(85)]);
     
     self.model = SerialLink(L,'name',name);
     
@@ -66,7 +66,7 @@ function PlotAndColourRobot(self)%robot,workspace)
     end
 
     % Display robot
-    self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+    self.model.plot3d([0,0,pi/4,pi/4,0,0],'noarrow','workspace',self.workspace);
     if isempty(findobj(get(gca,'Children'),'Type','Light'))
         camlight
     end  
